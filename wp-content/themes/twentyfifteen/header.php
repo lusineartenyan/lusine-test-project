0f36e896e82feedf087a537907457178247719a9
Lusine Artenyan <lusine.artenyan@gmail.com>
2015-09-03 22:39:00 +0400

    added new content to footer and header

--- wp-content/themes/twentyfifteen/header.php
+++ wp-content/themes/twentyfifteen/header.php
@@ -30,6 +30,7 @@
 			<div class="site-branding">
 				<?php
 					if ( is_front_page() && is_home() ) : ?>
+					<div>test1</div>
 						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<?php else : ?>
 						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
